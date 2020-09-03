90b714fe4be9312a3563d9ccc5225466164f4fc3
Scoinc <david_utbult@live.se>
2020-09-03 12:47:15 +0200

    e

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -3,9 +3,12 @@ const app = express()
 const port = 3000
 const clientDir = __dirname + "\\client\\"
 
+app.get('/cssen', (req, res) => {
+  res.sendFile(clientDir + "parallax.css")
+})
 app.get('/bild', (req, res) => {
-    res.sendFile(clientDir + "Kirby.jpg")
+    res.sendFile(clientDir + "attackOnKirby.png")
   })
-app.get('/', (req, res) => res.sendFile(clientDir + "index.html"))
+app.get('/', (req, res) => res.sendFile(clientDir + "parallax.html"))
 app.get('/teknik', (req, res) => res.send('e'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
