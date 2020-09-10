33e8d42707d7d5f7ccba00baeee0d5cf4ea324a6
Scoinc <david_utbult@live.se>
2020-09-10 13:30:37 +0200

    I don't get it why can't it read name

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -10,5 +10,11 @@ app.get('/bild', (req, res) => {
     res.sendFile(clientDir + "attackOnKirby.png")
   })
 app.get('/', (req, res) => res.sendFile(clientDir + "parallax.html"))
-app.post('/', (req, res) => res.sendFile(clientDir + "parallax.html"))
+
+app.post('/', (req, res) => {
+  console.log(req.body.name)
+  console.log(req.body.email)
+  res.redirect('/')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
